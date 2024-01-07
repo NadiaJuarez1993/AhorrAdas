2dfa8d16561219b32e3cc930d68e29229e93044b
Nadia Juarez <nadiajuarezz@hotmail.com>
2024-01-07 19:37:42 -0300

    adding function of categories

--- Javascript/main.js
+++ Javascript/main.js
@@ -21,8 +21,37 @@ const hideElement = (selectors) => {
 const getData = (key) => JSON.parse(localStorage.getItem(key));
 const setData = (key, data) => localStorage.setItem(key, JSON.stringify(data));
 
+//categorias pre establecidas
+const defaultCategories = [
+  {
+    id: randomId(), //genera id dinamico
+    categoryName: "Comida",
+  },
+  {
+    id: randomId(),
+    categoryName: "Servicios",
+  },
+  {
+    id: randomId(),
+    categoryName: "Salidas",
+  },
+  {
+    id: randomId(),
+    categoryName: "Educacion",
+  },
+  {
+    id: randomId(),
+    categoryName: "Transporte",
+  },
+  {
+    id: randomId(),
+    categoryName: "Trabajo",
+  }
+];
+
 //INICIALIZACION DE NUESTROS USUARIOS
 const allOperations = getData("operations") || []; //logica para pintar tabla: PEDIMOS INFO AL LOCAL STORAGE, SI TRAE INFO SE GUARDA EN VARIABLE ALL USERS Y SI NO SE CUMPLE SE GUARDA EN EL ARRAY VACIO
+const allCategories = getData("categories") || defaultCategories; //sino hay nuevas categorias , carga las categorias por defaoult
 
 /*RENDERS*/
 //SE ENCARGA DE RECIBIR LA OPERERACIONES COMO PARAMETROS PARA QE PODAMOS RECORRERER LA CANTIDAD DE OPERACIONES QUE HAYA CON ESTE BUCLE Y GENERAMOS LA TABLA
@@ -32,10 +61,11 @@ const renderOperations = (operations) => {
     hideElement(["#without-operations"]);
     showElement(["#width-operations"]);
     for (const operation of operations) {
+      const categorySelected = getData ("categories").find(category => category.id === operation.category)
       $("#operation-table-body").innerHTML += `
        <tr>
          <td class="py-4 font-semibold">${operation.description}</td>
-         <td class="text-green-400 py-2">${operation.category}</td>
+         <td class="text-green-400 py-2">${categorySelected.categoryName}</td>
          <td class="py-4">${operation.date}</td>
          <td class="py-4"> <span class="text-green-500">+$</span><span class="text-red-700 ">-$</span> ${operation.amount}</td>
          <td class="py-4">
@@ -51,6 +81,29 @@ const renderOperations = (operations) => {
   }
 };
 
+const renderCategoriesTable = (categories) => {
+   for (const category of categories) {
+     $("#table-category").innerHTML += `
+       <tr>
+         <td>${category.categoryName}</td>
+         <td>
+             <button class="rounded-none bg-inherit text-blue-600 hover:text-black"><a>Editar</a></button>  
+             <button class="rounded-none bg-inherit text-blue-600 hover:text-black"><a>Eliminar</a></button>
+          </td>
+     </tr>
+    `;
+   }
+ };
+
+const renderCategoryOptions = (categories) => {
+   for (const category of categories) {
+     $("#category-input").innerHTML += `
+     <option value="${category.id}">${category.categoryName}</option>
+     `;
+   }
+};
+
+/*DATA HANDLERS*/
 //Agregar operaciones
 const addOperation = () => {
   const currentData = getData("operations"); // pido la info
@@ -130,7 +183,10 @@ const deleteOperation = (operationId) => {
 /*EVENTS*/
 const initializeApp = () => {
   setData("operations", allOperations);
-  renderOperations(allOperations); //lo renderizo con lo que haya
+  setData("categories", allCategories);
+  renderOperations(allOperations); 
+  renderCategoriesTable(allCategories);
+  renderCategoryOptions(allCategories);
 
   $("#new-operation-btn").addEventListener("click", () => {
     // cambio de pantalla para agregar nueva operacion
@@ -181,14 +237,12 @@ const initializeApp = () => {
   $("#btn-hamburguer-menu").addEventListener("click", () => {
     showElement(["#nav-bar", "#btn-close-menu"]);
     hideElement(["#btn-hamburguer-menu"]);
-    
   });
 
   $("#btn-close-menu").addEventListener("click", () => {
     showElement(["#btn-hamburguer-menu"]);
-    hideElement(["#nav-bar","#btn-close-menu"]); 
+    hideElement(["#nav-bar", "#btn-close-menu"]);
   });
-
 };
 
 window.addEventListener("load", initializeApp);
